a83e54af39925b8a5ac61ba5a1a1b22cdf20c22c
MaximumOverflow <maximumoverflow904@gmail.com>
2024-02-14 17:30:39 +0100

    Lazy no more! ...the statics, not me
    Lazy statics are generally considered bad practice unless they're used for performance reasons.

--- src/resource_manager.rs
+++ src/resource_manager.rs
@@ -1,14 +1,13 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use actix_files::NamedFile;
-use crate::ROOT_PATH;
 
-pub fn get_file(path: &Path) -> NamedFile {
+pub fn get_file(path: &Path, root: &Path) -> NamedFile {
     let clean_path = path.to_str().unwrap().to_string().replace("\\", "/")
         .replace("../", "")
         .replace("./", "")
         .replace("%20", " ");
-    let mut final_path = PathBuf::from(ROOT_PATH.clone());
 
+    let mut final_path = root.to_path_buf();
     final_path.push("www");
     final_path.push(clean_path);
     NamedFile::open(final_path).expect(format!("Failed to open {:?}", path).as_str())
